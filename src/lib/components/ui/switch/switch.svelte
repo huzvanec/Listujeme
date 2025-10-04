1e72324c36b5a9806582121ca67c5f3f8f371305
Hužva <huzvanec@email.cz>
2025-10-04 23:57:00 +0200

    chore: update ui components

--- src/lib/components/ui/switch/switch.svelte
+++ src/lib/components/ui/switch/switch.svelte
@@ -1,38 +1,29 @@
-<script lang="ts" module>
-	import { Switch as SwitchPrimitive, type WithoutChildrenOrChild } from 'bits-ui';
-	import type { Snippet } from 'svelte';
-
-	export interface SwitchProps extends WithoutChildrenOrChild<SwitchPrimitive.RootProps> {
-		children?: Snippet;
-	}
-</script>
-
 <script lang="ts">
-	import { cn } from '$lib/utils.js';
+	import { Switch as SwitchPrimitive } from "bits-ui";
+	import { cn, type WithoutChildrenOrChild } from "$lib/utils.js";
 
 	let {
 		ref = $bindable(null),
 		class: className,
 		checked = $bindable(false),
-		children,
 		...restProps
-	}: SwitchProps = $props();
+	}: WithoutChildrenOrChild<SwitchPrimitive.RootProps> = $props();
 </script>
 
 <SwitchPrimitive.Root
 	bind:ref
 	bind:checked
+	data-slot="switch"
 	class={cn(
-		'peer inline-flex h-6 w-11 shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent transition-colors focus-visible:outline-hidden focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=unchecked]:bg-input',
+		"data-[state=checked]:bg-primary data-[state=unchecked]:bg-input focus-visible:border-ring focus-visible:ring-ring/50 dark:data-[state=unchecked]:bg-input/80 shadow-xs peer inline-flex h-[1.15rem] w-8 shrink-0 items-center rounded-full border border-transparent outline-none transition-all focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50",
 		className
 	)}
 	{...restProps}
 >
 	<SwitchPrimitive.Thumb
+		data-slot="switch-thumb"
 		class={cn(
-			'pointer-events-none inline-flex size-5 items-center justify-center rounded-full bg-background shadow-lg ring-0 transition-transform data-[state=checked]:translate-x-5 data-[state=unchecked]:translate-x-0 [&_svg]:size-3.5 [&_svg]:shrink-0'
+			"bg-background dark:data-[state=unchecked]:bg-foreground dark:data-[state=checked]:bg-primary-foreground pointer-events-none block size-4 rounded-full ring-0 transition-transform data-[state=checked]:translate-x-[calc(100%-2px)] data-[state=unchecked]:translate-x-0"
 		)}
-	>
-		{@render children?.()}
-	</SwitchPrimitive.Thumb>
+	/>
 </SwitchPrimitive.Root>
